464ff0085b25935b234f7f58b455440f9fe7ce38
MarcCDJ <marccruzdj@gmail.com>
2023-08-22 16:29:03 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Marc Cruz on 8/22/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
